0353e6533dd094e1a157acfee94a53d628ccaa64
Zishan <kenzeesh@gmail.com>
2026-04-14 03:51:38 +0100

    made changes to variables in main.tf (root directory) - passed var for child directory

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,5 +7,6 @@ provider "azurerm"{
 module "resource_group" {
   source              = "./modules/resource_group"
   resource_group_name = var.resource_group_name
+  vm_name = var.vm_name
   location            = "East US"
 }
