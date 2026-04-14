f963514a8b6069f1d24873d828c930150ce14847
Zishan <kenzeesh@gmail.com>
2026-04-14 02:36:24 +0100

    updated script file to run each file on specefic vm matrix and created github workflow file which run on local runner on workflow_dispatch i.e on clicking button

--- ansible/inventory.sh
+++ ansible/inventory.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 address=$(<public_ip.txt )
 mv private_key.pem /tmp/private_key.pem
-chmod 700 /tmp/private_key.pem
+chmod 600 /tmp/private_key.pem
 echo "ssh-host ansible_host=${address} ansible_user=azureuser ansible_ssh_private_key_file=/tmp/private_key.pem" >>inventory.ini
--- scripts/Benchmarking.sh
+++ scripts/Benchmarking.sh
@@ -1,8 +1,27 @@
 #!/bin/bash 
 mkdir -p ./results
-/usr/bin/time -v javac ./codes/Multiprocessing.java 2>&1 | tee ./results/compilation_java.txt
-g++  ./codes/Multiprocessing.cpp -o ./codes/Multiprocessing_c++ 
-/usr/bin/time -v ./codes/Multiprocessing_c++ 2>&1 | tee ./results/execution_cpp.txt
-gcc  ./codes/Multiprocessing.c  -o ./codes/Multiprocessing_c 
-/usr/bin/time -v ./codes/Multiprocessing_c 2>&1 | tee ./results/execution_c.txt
-/usr/bin/time -v python3 ./codes/Multiprocessing.py 2>&1 | tee ./results/execution_python.txt
+
+TARGET="${1:-}"
+case "$TARGET" in 
+    java)
+        /usr/bin/time -v javac ./codes/Multiprocessing.java 2>&1 | tee ./results/compilation_java.txt
+        ;;
+    java-virtual)
+        /usr/bin/time -v javac ./codes/Multiprocessing_Virtual.java 2>&1 | tee ./results/execution_java_virtual.txt
+        ;;
+    cpp)
+        g++  ./codes/Multiprocessing.cpp -o ./codes/Multiprocessing_c++ 
+        /usr/bin/time -v ./codes/Multiprocessing_c++ 2>&1 | tee ./results/execution_cpp.txt
+        ;;
+    c)
+        gcc  ./codes/Multiprocessing.c  -o ./codes/Multiprocessing_c 
+        /usr/bin/time -v ./codes/Multiprocessing_c 2>&1 | tee ./results/execution_c.txt
+        ;;
+    python)
+        /usr/bin/time -v python3 ./codes/Multiprocessing.py 2>&1 | tee ./results/execution_python.txt
+        ;;
+    *)
+        echo "Unknown benchmark target: $TARGET"
+        exit 1
+        ;;
+esac
