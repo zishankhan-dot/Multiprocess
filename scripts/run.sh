152ecde119863c7e3af070be6ab509526c544222
Zishan <kenzeesh@gmail.com>
2026-04-15 17:54:17 +0100

    update for running the matrix now with updated run.sh which will download required compiler depending on matrix

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-cd ./scripts
-top -b -n 1 >> top_output.txt
+TARGET="${1:-}"
+case "$TARGET" in
+    java || java-virtual)
+        sudo apt install -y default-jdk
+        java -version
+        ;;
+    cpp || c)
+        sudo apt install -y build-essential
+        ;;
+    python)
+        sudo apt install -y python3
+        python3 --version
+        ;;
+    *)
+        echo "Unknown target: $TARGET"
+        exit 1
+        ;;
+esac    
